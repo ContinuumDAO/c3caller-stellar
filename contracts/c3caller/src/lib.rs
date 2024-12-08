ebb4be6b73e18cdda155f55c157faf20f7356f9f
David Kathoh <mudavidkat@gmail.com>
2024-12-08 22:28:27 +0200

    removed an used imports in libs

--- contracts/c3caller/src/lib.rs
+++ contracts/c3caller/src/lib.rs
@@ -1,7 +1,4 @@
 #![no_std]
-use events::{LogC3CallEvent, LogExecCallEvent, LogExecFallbackEvent};
-use soroban_sdk::{contract, contractimpl, log, vec, Address, Env, IntoVal, String, Symbol, Val, Vec};
-
 mod events;
 mod uuid_keeper;
 mod tests;
